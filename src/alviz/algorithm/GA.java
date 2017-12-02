ccd16da2262ab7470f32dae04ca1fe2f8eee853a
aman935 <singh.amansingh935@gmail.com>
2017-12-03 02:37:47 +0530

    About to add PMX

--- src/alviz/algorithm/GA.java
+++ src/alviz/algorithm/GA.java
@@ -85,6 +85,47 @@ public class GA {
         }
         return child;
     }
+    // Applies crossover to a set of parents and creates offspring
+    public static Tour pmxCrossover(Tour parent1, Tour parent2) {
+        // Create new child tour
+        Tour child = new Tour();
+
+        // Get start and end sub tour positions for parent1's tour
+        int startPos = (int) (Math.random() * parent1.tourSize());
+        int endPos = (int) (Math.random() * parent1.tourSize());
+
+        // Loop and add the sub tour from parent1 to our child
+        for (int i = 0; i < child.tourSize(); i++) {
+            // If our start position is less than the end position
+            if (startPos < endPos && i > startPos && i < endPos) {
+                child.setCity(i, parent1.getCity(i));
+            } // If our start position is larger
+            else if (startPos > endPos) {
+                if (!(i < startPos && i > endPos)) {
+                    child.setCity(i, parent1.getCity(i));
+                }
+            }
+        }
+
+        // Loop through parent2's city tour
+        for (int i = 0; i < parent2.tourSize(); i++) {
+            // If child doesn't have the city add it
+            if (!child.containsCity(parent2.getCity(i))) {
+                // Loop to find a spare position in the child's tour
+                for (int ii = 0; ii < child.tourSize(); ii++) {
+                    // Spare position found, add city
+                    if (child.getCity(ii) == null) {
+                        child.setCity(ii, parent2.getCity(i));
+                        break;
+                    }
+                }
+            }
+            else{
+              
+            }
+        }
+        return child;
+    }
 
     // Mutate a tour using swap mutation
     private static void mutate(Tour tour) {
